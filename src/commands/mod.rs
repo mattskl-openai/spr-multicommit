7e011507a76798e4e38ff7eaa3ac4f9301e406a6
Matt Lee <mattskl@openai.com>
2025-08-14 21:31:10 -0700

    Add `land flatten` option, which squash merges all prs as single commit.
    pr:laneFlatten

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -6,7 +6,7 @@ pub mod restack;
 pub mod update;
 
 pub use fix_chain::fix_chain;
-pub use land::land_prs_until;
+pub use land::{land_flatten_until, land_per_pr_until};
 pub use list::list_prs_display;
 pub use prep::prep_squash;
 pub use restack::restack_existing;
