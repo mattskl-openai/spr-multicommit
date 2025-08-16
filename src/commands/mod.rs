f7a61c33c66adf83ab8f9e52d23c391bee93ebf9
Matt Lee <mattskl@openai.com>
2025-08-16 10:15:45 -0700

    fmt

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,16 +1,16 @@
-pub mod relink_stack;
 pub mod land;
 pub mod list;
 pub mod r#move;
 pub mod prep;
+pub mod relink_stack;
 pub mod restack;
 pub mod update;
 
-pub use relink_stack::relink_stack;
 pub use land::{land_flatten_until, land_per_pr_until};
 pub use list::list_commits_display;
 pub use list::list_prs_display;
 pub use prep::prep_squash;
 pub use r#move::move_groups_after;
+pub use relink_stack::relink_stack;
 pub use restack::restack_after;
 pub use update::build_from_tags;
